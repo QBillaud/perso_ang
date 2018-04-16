4542fec2a7b05094e8c6ffd79e75a460a31502e9
QBillaud <quentin.billaud@telecomnancy.eu>
2018-04-16 02:46:02 +0200

    CrypticStarting

--- src/application/mainFrameController.java
+++ src/application/mainFrameController.java
@@ -1,15 +1,24 @@
 package application;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class mainFrameController {
+
+	int num = 0;
+	int numDecal = 0;
+	csv_reader R;
+	ObservableList<String> correctWords;
 
 	@FXML
 	private AnchorPane mainScreen;
@@ -30,7 +39,28 @@ public class mainFrameController {
 	private Button OptionsB;
 
 	@FXML
+	private Button helpB;
+
+	@FXML
+	private Button validateB;
+
+	@FXML
+	private Button skipB;
+
+	@FXML
+	private ComboBox lessonSelect;
+
+	@FXML
+	private Label def;
+
+	@FXML
+	private Label hint;
+
+	@FXML
 	private ListView<String> lessonsL;
+
+	@FXML
+	private TextField answerField;
 
 	public mainFrameController() {
 
@@ -38,14 +68,36 @@ public class mainFrameController {
 
 	public void showLessons() throws FileNotFoundException{
 		mainScreen.setVisible(false);
-		ObservableList<String> lessonsList = FXCollections.observableArrayList();
-		csv_reader R = new csv_reader("src/application/files/list.csv");
-		for (int i=1; i<=Integer.parseInt(R.vocFetcher(0,0)); i++){
-			lessonsList.addAll(R.vocFetcher(0,i));
-		}
-		System.out.println(lessonsList);
-		lessonsL.setItems(lessonsList);
+
+		num = 0;
+		numDecal = 0;
+
+		csv_reader R = new csv_reader("src/application/files/l0.csv");
+		def.setText(R.vocFetcher(num,1));
 		lessonsScreen.setVisible(true);
+	}
+
+	public void validateWord(){
+		if(answerField.getText().equals(R.vocFetcher(num,0))){
+			if (num!=19){
+				hint.setText("Correct !");
+				correctWords.add(R.vocFetcher(num,0));
+				lessonsL.setItems(correctWords);
+				num++;
+				numDecal++;
+				def.setText(R.vocFetcher(num,1));
+			}
+			else {
+				String awt = "Your Score is" + Integer.toString(numDecal);
+				def.setText("You finished all the words !");
+				hint.setText(awt);
+				correctWords.add(R.vocFetcher(num,0));
+				lessonsL.setItems(correctWords);
+			}
+		}
+		else{
+			hint.setText("Not Quite. Try again !");
+		}
 	}
 
 	public void showGlossary(){
@@ -59,5 +111,4 @@ public class mainFrameController {
 	public void showOptions(){
 		mainScreen.setVisible(false);
 	}
-
 }
